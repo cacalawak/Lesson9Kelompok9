82e9ad31744409a38ad8fb34cd2cad94a01aee4b
zaxxy666 <ihsaanzaky@gmail.com>
2023-10-14 15:36:06 +0700

    Peran ViewModel adalah memberikan data ke UI dan mempertahankan perubahan konfigurasi. ViewModel bertindak sebagai pusat komunikasi antara Repositori dan UI. Anda juga dapat menggunakan ViewModel untuk berbagi data di antara fragmen.

---
+++ app/src/main/java/com/example/myapplicationmobilelesson9/WordViewModel.kt
@@ -0,0 +1,36 @@
+package com.example.myapplicationmobilelesson9
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class WordViewModel {
+    class WordViewModel(private val repository: WordRepository) : ViewModel() {
+
+        // Using LiveData and caching what allWords returns has several benefits:
+        // - We can put an observer on the data (instead of polling for changes) and only update the
+        //   the UI when the data actually changes.
+        // - Repository is completely separated from the UI through the ViewModel.
+        val allWords: LiveData<List<Word>> = repository.allWords.asLiveData()
+
+        /**
+         * Launching a new coroutine to insert the data in a non-blocking way
+         */
+        fun insert(word: Word) = viewModelScope.launch {
+            repository.insert(word)
+        }
+    }
+
+    class WordViewModelFactory(private val repository: WordRepository) : ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(WordViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return WordViewModel(repository) as T
+            }
+            throw IllegalArgumentException("Unknown ViewModel class")
+        }
+    }
+}
