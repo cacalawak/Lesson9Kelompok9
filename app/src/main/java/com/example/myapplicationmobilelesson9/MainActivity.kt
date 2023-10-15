0b706e6d6db53fc33d98eaccefe42c3c8c49d4e8
leon_Galliard <fvkleonsan@gmail.com>
2023-10-15 09:49:45 +0700

    display the data in a RecyclerView, which is a little nicer than just throwing the data in a TextView. This codelab assumes that we know how RecyclerView, RecyclerView.ViewHolder, and ListAdapter

--- app/src/main/java/com/example/myapplicationmobilelesson9/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationmobilelesson9/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.myapplicationmobilelesson9
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        val adapter = WordListAdapter()
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
